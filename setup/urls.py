1b02ea6256cdc8810447f19c8351f017b33a0aaf
Victor Hugo Morais Santos <victormacm1@MacBook-Pro-de-Victor.local>
2024-11-26 14:20:23 -0300

    add: matriculas

--- setup/urls.py
+++ setup/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
-from escola.views import EstudanteViewSet,CursoViewSet
+from escola.views import EstudanteViewSet,CursoViewSet,MatriculaViewSet
 from rest_framework import routers
 
 routers = routers.DefaultRouter()
 routers.register('estudantes', EstudanteViewSet, basename='Estudantes')
 routers.register('cursos', CursoViewSet, basename='Cursos')
+routers.register('matriculas', MatriculaViewSet, basename='Matriculas')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
